949fb095731841a99787678fe79e75bc878a4ae1
ArisQuraisy <ariezquraisy@gmail.com>
2020-04-25 18:42:08 +0700

    first commit

---
+++ t.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+	<title>form sederhana</title>
+</head>
+<body>
+<form method="post"action="tt.php">
+	<table>
+		<tr>
+			<td>Nama Wilayah</td>
+			<td>:</td>
+		<td>
+			<select size "1" name="namawilayah">
+			<option>DKI jakarta
+			<option>Jawa barat
+			<option>Banten
+			<option>Jawa Tengah	
+		</select>
+		</td>
+		</tr>
+		<tr>
+			<td>Jumlah Positif</td>
+			<td>:</td>
+			<td><input type="text" name="jumlahp"></td>
+		</tr>
+		<tr>
+			<td>Jumlah Dirawat</td>
+			<td>:</td>
+			<td><input type="text" name="jumlahdirwt"></td>
+		</tr>
+		<tr>
+			<td>Jumlah Sembuh</td>
+			<td>:</td>
+			<td><input type="text" name="jumlahsmbh"></td>
+		</tr>
+		<tr>
+			<td>Jumlah Meninggal</td>
+			<td>:</td>
+			<td><input type="text" name="jumlahm"></td>
+		</tr>
+		<tr>
+			<td>Nama Operator</td>
+			<td>:</td>
+			<td><input type="text" name="namaop"></td>
+		</tr>
+		<tr>
+			<td>Nim Mahasiswa</td>
+			<td>:</td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" value="input"></td>
+			
+
+		</tr>
+	</table>
+</form>
+</body>
+</html>
---
+++ tt.php
@@ -0,0 +1,13 @@
+<?php
+$nama = $_POST["namawilayah"];
+$jumlahpositif = $_POST["jumlahp"];
+$jumlahdirawat = $_POST["jumlahdirwt"];
+$jumlahsembuh = $_POST["jumlahsmbh"];
+$jumlahmeninggal = $_POST["jumlahm"];
+$namaoperator = $_POST["namaop"];
+$nimmahasiswa = $_POST["nim"];
+
+echo "namawilayah = $nama <br> jumlahpositif = $jumlahpositif <br> jumlahdirawat = $jumlahdirawat <br>
+jumlahsembuh = $jumlahsembuh <br> jumlahmeninggal = $jumlahmeninggal <br> namaoperator = $namaoperator <br> nimmahasiswa = $nimmahasiswa";
+
+?>
